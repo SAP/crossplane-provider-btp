bc1a0c42c1a0c65e211246008635cfc1f77b31eb
I545514 <gazelle.zaheer@sap.com>
2024-11-25 15:49:24 +0100

    chore: e2e tes

--- internal/controller/zz_setup.go
+++ internal/controller/zz_setup.go
@@ -22,6 +22,7 @@ import (
 	"github.com/crossplane/upjet/pkg/controller"
 
 	directoryentitlement "github.com/sap/crossplane-provider-btp/internal/controller/account/directoryentitlement"
+	subaccountservicebroker "github.com/sap/crossplane-provider-btp/internal/controller/account/subaccountservicebroker"
 	providerconfig "github.com/sap/crossplane-provider-btp/internal/controller/providerconfig"
 	globalaccounttrustconfiguration "github.com/sap/crossplane-provider-btp/internal/controller/security/globalaccounttrustconfiguration"
 	subaccountapicredential "github.com/sap/crossplane-provider-btp/internal/controller/security/subaccountapicredential"
@@ -33,6 +34,7 @@ import (
 func Setup(mgr ctrl.Manager, o controller.Options) error {
 	for _, setup := range []func(ctrl.Manager, controller.Options) error{
 		directoryentitlement.Setup,
+		subaccountservicebroker.Setup,
 		providerconfig.Setup,
 		globalaccounttrustconfiguration.Setup,
 		subaccountapicredential.Setup,
