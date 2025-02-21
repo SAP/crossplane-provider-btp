3c88f96f9342429d70930c22860151d09b617e65
Louis Kauer <laky.dev@proton.me>
2025-02-21 16:31:00 +0100

    refactor: updated function names and docs in tests

--- internal/clients/cfenvironment/cfenvironment_test.go
+++ internal/clients/cfenvironment/cfenvironment_test.go
@@ -4,8 +4,8 @@ import (
 	"github.com/sap/crossplane-provider-btp/apis/environment/v1alpha1"
 )
 
-func crWithManagers(specManagers []string, statusManagers []string) v1alpha1.CloudFoundryEnvironment {
-
+// crWithManagers returns a CloudFoundryEnvironment CR with the given managers.
+func _(specManagers []string, statusManagers []string) v1alpha1.CloudFoundryEnvironment {
 	return v1alpha1.CloudFoundryEnvironment{
 		Spec: v1alpha1.CfEnvironmentSpec{
 			ForProvider: v1alpha1.CfEnvironmentParameters{
@@ -20,11 +20,11 @@ func crWithManagers(specManagers []string, statusManagers []string) v1alpha1.Clo
 	}
 }
 
+// toUserSlice converts a slice of strings to a slice of v1alpha1.User.
 func toUserSlice(ss []string) []v1alpha1.User {
 	us := make([]v1alpha1.User, 0)
 	for _, s := range ss {
 		us = append(us, v1alpha1.User{Username: s, Origin: "sap.ids"})
 	}
-
 	return us
 }
