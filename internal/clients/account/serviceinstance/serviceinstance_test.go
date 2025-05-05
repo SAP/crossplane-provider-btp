b4ebd248efc5e12281a054c0a804918a2bb86634
Stephan Discher <stephan.discher@sap.com>
2025-05-05 11:00:11 +0200

    feat: add success testing for creator Connect

--- internal/clients/account/serviceinstance/serviceinstance_test.go
+++ internal/clients/account/serviceinstance/serviceinstance_test.go
@@ -17,11 +17,11 @@ func TestConnect(t *testing.T) {
 		cr *v1alpha1.ServiceInstance
 	}
 	type fields struct {
-		connector managed.ExternalConnecter
+		connector *TfConnectorMock
 	}
 	type want struct {
-		client *ServiceInstanceClient
-		err    error
+		clientReturned bool
+		err            error
 	}
 	tests := []struct {
 		name   string
@@ -40,8 +40,23 @@ func TestConnect(t *testing.T) {
 				},
 			},
 			want: want{
-				client: nil,
-				err:    tfError,
+				clientReturned: false,
+				err:            tfError,
+			},
+		},
+		{
+			name: "SuccessfulConnect",
+			args: args{
+				cr: &v1alpha1.ServiceInstance{},
+			},
+			fields: fields{
+				connector: &TfConnectorMock{
+					err: nil,
+				},
+			},
+			want: want{
+				clientReturned: true,
+				err:            nil,
 			},
 		},
 	}
@@ -50,11 +65,18 @@ func TestConnect(t *testing.T) {
 			clientCreator := &ServiceInstanceClientCreator{
 				connector: tc.fields.connector,
 			}
-			_, err := clientCreator.Connect(context.Background(), tc.args.cr)
+			client, err := clientCreator.Connect(context.Background(), tc.args.cr)
 			if err != nil && err.Error() != tc.want.err.Error() {
 				t.Errorf("ServiceInstanceClientCreator.Connect() error = %v, want %v", err, tc.want.err)
 			}
+			if (client != nil) != tc.want.clientReturned {
+				t.Errorf("ServiceInstanceClientCreator.Connect() client returned = %v, want %v", client != nil, tc.want.clientReturned)
+			}
 
+			// Verify that the Connect method was called with the correct type
+			if _, ok := tc.fields.connector.CalledWithMg.(*v1alpha1.SubaccountServiceInstance); !ok {
+				t.Errorf("ServiceInstanceClientCreator.Connect() called with wrong type %T, want %T", tc.fields.connector.CalledWithMg, &v1alpha1.SubaccountServiceInstance{})
+			}
 		})
 	}
 }
@@ -63,11 +85,13 @@ var _ managed.ExternalConnecter = &TfConnectorMock{}
 
 // ClientCreator mock
 type TfConnectorMock struct {
-	err error
+	err          error
+	CalledWithMg resource.Managed // Field to store the resource passed to Connect
 }
 
 // Connect implements managed.ExternalConnecter.
 func (t *TfConnectorMock) Connect(ctx context.Context, mg resource.Managed) (managed.ExternalClient, error) {
+	t.CalledWithMg = mg // Store the resource passed to Connect
 	if t.err != nil {
 		return nil, t.err
 	}
