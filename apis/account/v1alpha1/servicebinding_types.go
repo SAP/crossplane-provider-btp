405a756ffa9daab3901ba65105339eda0b2de324
Stephan Discher <stephan.discher@sap.com>
2025-06-05 13:49:39 +0200

    docs: make type docs more usefull

--- apis/account/v1alpha1/servicebinding_types.go
+++ apis/account/v1alpha1/servicebinding_types.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2022 The Crossplane Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1alpha1
 
 import (
@@ -92,7 +76,7 @@ type ServiceBindingStatus struct {
 
 // +kubebuilder:object:root=true
 
-// A ServiceBinding is an example API type.
+// A ServiceBinding allows to manage a binding to a service instance in BTP
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
