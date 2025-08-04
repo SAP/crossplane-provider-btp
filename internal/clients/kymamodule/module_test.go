bac9d07b7a2a6862135569f8a2204e4ad0723798
Lasserich <lasse.friedrich@sap.com>
2025-08-04 15:11:19 +0200

    feat: unit test extensions

--- internal/clients/kymamodule/module_test.go
+++ internal/clients/kymamodule/module_test.go
@@ -2,21 +2,22 @@ package kymamodule
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/crossplane/crossplane-runtime/pkg/test"
 	"github.com/google/go-cmp/cmp"
 	"github.com/sap/crossplane-provider-btp/apis/environment/v1alpha1"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestGetDefaultKyma(t *testing.T) {
 
 	type args struct {
-		obj *v1alpha1.KymaCr
+		obj    *v1alpha1.KymaCr
+		client *KymaModuleClient
 	}
 
 	type want struct {
@@ -31,10 +32,25 @@ func TestGetDefaultKyma(t *testing.T) {
 		"Happy Path": {
 			args: args{
 				obj: kymaCr(withSpec(v1alpha1.KymaSpec{})),
+				client: &KymaModuleClient{
+					kube: &test.MockClient{MockGet: test.NewMockGetFn(nil)},
+				},
 			},
 			want: want{
-				payload: kymaCr(),
+				payload: kymaCr(withGVK(GVKKyma)),
 				err:     nil,
+			},
+		},
+		"Boom!": {
+			args: args{
+				obj: kymaCr(withSpec(v1alpha1.KymaSpec{})),
+				client: &KymaModuleClient{
+					kube: &test.MockClient{MockGet: test.NewMockGetFn(errors.New("BOOM"))},
+				},
+			},
+			want: want{
+				payload: nil,
+				err:     errors.New("BOOM"),
 			},
 		},
 	}
@@ -43,9 +59,7 @@ func TestGetDefaultKyma(t *testing.T) {
 		t.Run(
 			name, func(t *testing.T) {
 
-				kymaCR, err := getDefaultKyma(context.TODO(), &KymaModuleClient{
-					kube: &test.MockClient{MockGet: test.NewMockGetFn(nil)},
-				})
+				kymaCR, err := getDefaultKyma(context.TODO(), tc.args.client)
 
 				if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
 					t.Errorf("\nKymaModuleClient\n.getDefaultKyma(...): -want error, +got error:\n%s\n", diff)
@@ -65,7 +79,8 @@ type kymaModifier func(kymaModule *v1alpha1.KymaCr)
 func kymaCr(m ...kymaModifier) *v1alpha1.KymaCr {
 	cr := &v1alpha1.KymaCr{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "kymaCr",
+			Name:      DefaultKymaName,
+			Namespace: DefaultKymaNamespace,
 		},
 	}
 	for _, f := range m {
@@ -74,46 +89,14 @@ func kymaCr(m ...kymaModifier) *v1alpha1.KymaCr {
 	return cr
 }
 
-func withConditions(c ...xpv1.Condition) kymaModifier {
-	return func(r *v1alpha1.KymaCr) {}
-}
-func withUID(uid types.UID) kymaModifier {
-	return func(r *v1alpha1.KymaCr) { r.UID = uid }
-}
-
-func withStatus(status v1alpha1.KymaStatus) kymaModifier {
+func withGVK(gvk schema.GroupVersionKind) kymaModifier {
 	return func(r *v1alpha1.KymaCr) {
-		r.Status = status
+		r.SetGroupVersionKind(gvk)
 	}
 }
+
 func withSpec(spec v1alpha1.KymaSpec) kymaModifier {
 	return func(r *v1alpha1.KymaCr) {
 		r.Spec = spec
-	}
-}
-
-func withAnnotaions(annotations map[string]string) kymaModifier {
-	return func(r *v1alpha1.KymaCr) {
-		r.ObjectMeta.Annotations = annotations
-	}
-}
-
-func kymaDefaultSpec() v1alpha1.KymaSpec {
-	return v1alpha1.KymaSpec{
-		Channel: "fast",
-		Modules: []v1alpha1.Module{
-			{
-				Name:                 "istio",
-				CustomResourcePolicy: "CreateAndDelete",
-			},
-			{
-				Name:                 "api-gateway",
-				CustomResourcePolicy: "CreateAndDelete",
-			},
-			{
-				Name:                 "btp-operator",
-				CustomResourcePolicy: "CreateAndDelete",
-			},
-		},
 	}
 }
--- internal/controller/kymamodule/kymamodule_test.go
+++ internal/controller/kymamodule/kymamodule_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"testing"
 
-	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
@@ -14,7 +14,6 @@ import (
 	"github.com/sap/crossplane-provider-btp/apis/environment/v1alpha1"
 	"github.com/sap/crossplane-provider-btp/internal/clients/kymamodule"
 	"github.com/sap/crossplane-provider-btp/internal/controller/kymamodule/fake"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 func TestObserve(t *testing.T) {
@@ -47,6 +46,32 @@ func TestObserve(t *testing.T) {
 				err: nil,
 			},
 		},
+		"Needs Creation": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockObserve: func(moduleCr *v1alpha1.KymaModule) (*v1alpha1.ModuleStatus, error) {
+					return nil, nil
+				}},
+			},
+			want: want{
+				cr:  module(),
+				obs: managed.ExternalObservation{ResourceExists: false},
+				err: nil,
+			},
+		},
+		"Boom!": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockObserve: func(moduleCr *v1alpha1.KymaModule) (*v1alpha1.ModuleStatus, error) {
+					return nil, errors.New("BOOM")
+				}},
+			},
+			want: want{
+				cr:  module(),
+				obs: managed.ExternalObservation{},
+				err: errors.New("BOOM"),
+			},
+		},
 	}
 
 	for name, tc := range cases {
@@ -63,44 +88,137 @@ func TestObserve(t *testing.T) {
 	}
 }
 
-func moduleStatus(state string) *v1alpha1.ModuleStatus {
-	return &v1alpha1.ModuleStatus{State: state}
+func TestCreate(t *testing.T) {
+	type args struct {
+		cr     resource.Managed
+		client kymamodule.Client
+	}
+
+	type want struct {
+		cr  resource.Managed
+		obs managed.ExternalCreation
+		err error
+	}
+
+	cases := map[string]struct {
+		args args
+		want want
+	}{
+		"Happy Path": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockCreate: func(moduleName string, moduleChannel string, customResourcePolicy string) error {
+					return nil
+				}},
+			},
+			want: want{
+				cr:  module(),
+				obs: managed.ExternalCreation{},
+				err: nil,
+			},
+		},
+		"Boom!": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockCreate: func(moduleName string, moduleChannel string, customResourcePolicy string) error {
+					return errors.New("BOOM")
+				}},
+			},
+			want: want{
+				cr:  module(),
+				obs: managed.ExternalCreation{},
+				err: errors.New("BOOM"),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			e := external{client: tc.args.client}
+			got, err := e.Create(context.Background(), tc.args.cr)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("\ne.Observe(...): -want error, +got error:\n%s\n", diff)
+			}
+			if diff := cmp.Diff(tc.want.obs, got); diff != "" {
+				t.Errorf("\ne.Observe(...): -want, +got:\n%s\n", diff)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	type args struct {
+		cr     resource.Managed
+		client kymamodule.Client
+	}
+
+	type want struct {
+		cr  resource.Managed
+		err error
+	}
+
+	cases := map[string]struct {
+		args args
+		want want
+	}{
+		"Happy Path": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockDelete: func(moduleName string) error {
+					return nil
+				}},
+			},
+			want: want{
+				cr:  module(),
+				err: nil,
+			},
+		},
+		"Boom!": {
+			args: args{
+				cr: module(),
+				client: &fake.MockKymaModuleClient{MockDelete: func(moduleName string) error {
+					return errors.New("BOOM")
+				}},
+			},
+			want: want{
+				cr:  module(),
+				err: errors.New("BOOM"),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			e := external{client: tc.args.client}
+			err := e.Delete(context.Background(), tc.args.cr)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("\ne.Observe(...): -want error, +got error:\n%s\n", diff)
+			}
+		})
+	}
 }
 
 type moduleModifier func(kymaModule *v1alpha1.KymaModule)
-
-func withConditions(c ...xpv1.Condition) moduleModifier {
-	return func(r *v1alpha1.KymaModule) { r.Status.ConditionedStatus.Conditions = c }
-}
-func withUID(uid types.UID) moduleModifier {
-	return func(r *v1alpha1.KymaModule) { r.UID = uid }
-}
-
-func withState(status string) moduleModifier {
-	return func(r *v1alpha1.KymaModule) {
-		r.Status.AtProvider.State = status
-	}
-}
-func withData(data v1alpha1.KymaModuleParameters) moduleModifier {
-	return func(r *v1alpha1.KymaModule) {
-		r.Spec.ForProvider = data
-	}
-}
-
-func withAnnotaions(annotations map[string]string) moduleModifier {
-	return func(r *v1alpha1.KymaModule) {
-		r.ObjectMeta.Annotations = annotations
-	}
-}
 
 func module(m ...moduleModifier) *v1alpha1.KymaModule {
 	cr := &v1alpha1.KymaModule{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "kymaModule",
 		},
+		Spec: v1alpha1.KymaModuleSpec{
+			ForProvider: v1alpha1.KymaModuleParameters{
+				Name:                 "testModule",
+				Channel:              ptrString("regular"),
+				CustomResourcePolicy: ptrString("createdelete"),
+			},
+		},
 	}
 	for _, f := range m {
 		f(cr)
 	}
 	return cr
+}
+
+func ptrString(s string) *string {
+	return &s
 }
