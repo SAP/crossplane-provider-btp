5f85c6b177f1b41d3799854514ab96f2a2680f1e
Enrico Kaack <enrico.kaack@sap.com>
2025-09-16 10:09:20 +0200

    Revert "chore(btp-tf-provider): upgrade to v1.16.1 (#329)" (#337)
    This reverts commit a9e5625173eb7b0808d90f01657f297f6d29809e.
    
    We can not yet upgrade to v1.16.x since it requires go 1.25 for the no-fork upjet and the build submodule is not updated yet.

--- apis/account/v1alpha1/zz_generated.deepcopy.go
+++ apis/account/v1alpha1/zz_generated.deepcopy.go
@@ -413,11 +413,6 @@ func (in *DirectoryEntitlementInitParameters) DeepCopyInto(out *DirectoryEntitle
 		*out = new(string)
 		**out = **in
 	}
-	if in.PlanUniqueIdentifier != nil {
-		in, out := &in.PlanUniqueIdentifier, &out.PlanUniqueIdentifier
-		*out = new(string)
-		**out = **in
-	}
 	if in.ServiceName != nil {
 		in, out := &in.ServiceName, &out.ServiceName
 		*out = new(string)
@@ -515,11 +510,6 @@ func (in *DirectoryEntitlementObservation) DeepCopyInto(out *DirectoryEntitlemen
 		*out = new(string)
 		**out = **in
 	}
-	if in.PlanUniqueIdentifier != nil {
-		in, out := &in.PlanUniqueIdentifier, &out.PlanUniqueIdentifier
-		*out = new(string)
-		**out = **in
-	}
 	if in.ServiceName != nil {
 		in, out := &in.ServiceName, &out.ServiceName
 		*out = new(string)
@@ -577,11 +567,6 @@ func (in *DirectoryEntitlementParameters) DeepCopyInto(out *DirectoryEntitlement
 	}
 	if in.PlanName != nil {
 		in, out := &in.PlanName, &out.PlanName
-		*out = new(string)
-		**out = **in
-	}
-	if in.PlanUniqueIdentifier != nil {
-		in, out := &in.PlanUniqueIdentifier, &out.PlanUniqueIdentifier
 		*out = new(string)
 		**out = **in
 	}
