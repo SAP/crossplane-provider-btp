eff1c181fc7d6a297374fdca859baec0f86bd6bb
SatabdiG <satabdi.ganguly89@gmail.com>
2025-10-15 12:07:58 +0200

    client: Implement KymaServiceInstance k8 client

--- internal/clients/kymaserviceinstance/client.go
+++ internal/clients/kymaserviceinstance/client.go
@@ -6,13 +6,25 @@ import (
 	"github.com/pkg/errors"
 
 	"github.com/sap/crossplane-provider-btp/apis/environment/v1alpha1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/tools/clientcmd"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
-	errFailedParse        = "failed to parse kubeconfig"
-	errFailedCreateClient = "failed to create Kubernetes client"
+	errFailedParse                        = "failed to parse kubeconfig"
+	errFailedCreateClient                 = "failed to create Kubernetes client"
+	errFailedToGetServiceInstance         = "failed to get service instance"
+	errFailedToConvertToServiceInstance   = "failed to convert ServiceInstance from unstructured"
+	errFailedToConvertFromServiceInstance = "failed to convert ServiceInstance to unstructured"
+	errFailedToCreateServiceInstance      = "failed to create ServiceInstance %s in %s"
+	errFailedToDeleteServiceInstance      = "failed to delete ServiceInstance %s in %s"
+	errFailedToGetExistingServiceInstance = "failed to get existing ServiceInstance for update"
+	errFailedToUpdateServiceInstance      = "failed to update ServiceInstance %s in %s"
 )
 
 // Client is the interface for managing ServiceInstances in Kyma clusters
@@ -51,30 +63,141 @@ var GVKServiceInstance = schema.GroupVersionKind{
 	Kind:    "ServiceInstance",
 }
 
-func (c *client) DescribeInstance(
-	ctx context.Context,
-	namespace, name string,
-) (*v1alpha1.KymaServiceInstanceObservation, bool, error) {
-	// TODO: Implement
-	return nil, false, errors.New("not implemented yet")
-}
-
 // Create a new client from kubeconfig bytes
 func NewKymaServiceInstanceClient(kubeconfigBytes []byte) (Client, error) {
-	return nil, errors.New("Not Implemented")
+	restClient, err := clientcmd.RESTConfigFromKubeConfig(kubeconfigBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, errFailedParse)
+	}
+	kubeClient, err := crclient.New(restClient, crclient.Options{})
+	if err != nil {
+		return nil, errors.Wrap(err, errFailedCreateClient)
+	}
+	return &client{kube: kubeClient}, nil
+}
+
+func (c *client) DescribeInstance(
+	ctx context.Context,
+	namespace,
+	name string,
+) (*v1alpha1.KymaServiceInstanceObservation, bool, error) {
+	// Get the ServiceInstance from Kyma
+	si, err := c.getServiceInstance(ctx, namespace, name)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, false, nil
+		}
+		return nil, false, errors.Wrap(err, errFailedToGetServiceInstance)
+	}
+
+	obs := &v1alpha1.KymaServiceInstanceObservation{
+		Ready:      si.Status.Ready,
+		InstanceID: si.Status.InstanceID,
+	}
+	// TODO: Check if external-name needs updating
+	externalNameUpdated := false
+
+	return obs, externalNameUpdated, nil
+
 }
 
 // Create a ServiceInstance in Kyma cluster
 func (c *client) CreateInstance(ctx context.Context, si *v1alpha1.KymaServiceInstance) error {
-	return errors.New("not implemented")
+	// Build the ServiceInstance object
+	newSI := &ServiceInstance{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: GVKServiceInstance.GroupVersion().String(),
+			Kind:       GVKServiceInstance.Kind,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      si.Spec.ForProvider.Name,
+			Namespace: si.Spec.ForProvider.Namespace,
+		},
+		Spec: ServiceInstanceSpec{
+			ServiceOfferingName: si.Spec.ForProvider.ServiceOfferingName,
+			ServicePlanName:     si.Spec.ForProvider.ServicePlanName,
+			ExternalName:        si.Spec.ForProvider.ExternalName,
+			Parameters:          si.Spec.ForProvider.Parameters,
+		},
+	}
+	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(newSI)
+	if err != nil {
+		return errors.Wrap(err, errFailedToConvertFromServiceInstance)
+	}
+	obj := &unstructured.Unstructured{Object: unstructuredObj}
+	// Create in Kyma cluster
+	if err := c.kube.Create(ctx, obj); err != nil {
+		return errors.Wrapf(err, errFailedToCreateServiceInstance,
+			si.Spec.ForProvider.Name, si.Spec.ForProvider.Namespace)
+	}
+	return nil
+
 }
 
 // Update a ServiceInstance in Kyma cluster
 func (c *client) UpdateInstance(ctx context.Context, si *v1alpha1.KymaServiceInstance) error {
-	return errors.New("not implemented")
+	name := si.Spec.ForProvider.Name
+	namespace := si.Spec.ForProvider.Namespace
+	existing, err := c.getServiceInstance(ctx, namespace, name)
+	if err != nil {
+		return errors.Wrap(err, errFailedToGetExistingServiceInstance)
+	}
+	//Update the spec fields
+	existing.Spec.ServiceOfferingName = si.Spec.ForProvider.ServiceOfferingName
+	existing.Spec.ServicePlanName = si.Spec.ForProvider.ServicePlanName
+	existing.Spec.ExternalName = si.Spec.ForProvider.ExternalName
+	existing.Spec.Parameters = si.Spec.ForProvider.Parameters
+
+	// Convert to unstructured
+	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(existing)
+	if err != nil {
+		return errors.Wrap(err, errFailedToConvertFromServiceInstance)
+	}
+	obj := &unstructured.Unstructured{Object: unstructuredObj}
+
+	// Update in Kyma cluster
+	if err := c.kube.Update(ctx, obj); err != nil {
+		return errors.Wrapf(err, errFailedToUpdateServiceInstance,
+			si.Spec.ForProvider.Name, si.Spec.ForProvider.Namespace)
+	}
+	return nil
 }
 
 // Delete a ServiceInstance from Kyma cluster
 func (c *client) DeleteInstance(ctx context.Context, namespace, name string) error {
-	return errors.New("not implemented")
+	// Delete from Kyma cluster
+	si := &unstructured.Unstructured{}
+	si.SetGroupVersionKind(GVKServiceInstance)
+	si.SetName(name)
+	si.SetNamespace(namespace)
+
+	if err := c.kube.Delete(ctx, si); err != nil {
+		if apierrors.IsNotFound(err) {
+			// Already Deleted
+			return nil
+		}
+		return errors.Wrapf(err, errFailedToDeleteServiceInstance, name, namespace)
+	}
+	return nil
+}
+
+func (c *client) getServiceInstance(ctx context.Context, namespace, name string) (*ServiceInstance, error) {
+	obj := &unstructured.Unstructured{}
+	obj.SetGroupVersionKind(GVKServiceInstance)
+
+	// Get key from Kyma Cluster
+	key := crclient.ObjectKey{
+		Namespace: namespace,
+		Name:      name,
+	}
+
+	if err := c.kube.Get(ctx, key, obj); err != nil {
+		return nil, err
+	}
+	// Convert to typed struct
+	si := &ServiceInstance{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.Object, si); err != nil {
+		return nil, errors.Wrap(err, errFailedToConvertToServiceInstance)
+	}
+	return si, nil
 }
